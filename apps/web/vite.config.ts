6e5f361eaabeec0ec82db8d9b2abaa0c8d55c07e
Ryan Luong <lumintriet@gmail.com>
2026-01-04 20:12:12 +0700

    chore(web): configure Vite.

---
+++ apps/web/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import path from "node:path";
+
+export default defineConfig({
+  plugins: [
+    tanstackStart(),
+    // react's vite plugin must come after start's vite plugin
+    viteReact(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
