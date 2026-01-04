63584245911b16876475174d5746995297afe958
Ryan Luong <lumintriet@gmail.com>
2026-01-04 21:12:06 +0700

    feat(web/ui): Button component.

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,13 +1,19 @@
+import { Button } from "@/components/ui/button";
 import { createFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
 
 export const Route = createFileRoute("/")({
   component: Home,
 });
 
 function Home() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
+    <div className="flex h-screen flex-col items-center justify-center gap-4">
       <h1 className="text-3xl font-bold">Home</h1>
+      <Button onClick={() => setCount((prev) => prev + 1)}>
+        Count: {count}
+      </Button>
     </div>
   );
 }
